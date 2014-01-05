a71ca22cccbf05bbc9b7de5d09c5eb4f9060fe2c
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-01-05 19:10:05 +0000

    Added another unit test re Player

--- src/jt/katas/tennis/Player.scala
+++ src/jt/katas/tennis/Player.scala
@@ -3,4 +3,6 @@ package jt.katas.tennis
 /**
  * Created by Jonathan Taylor on 04/01/2014.
  */
-class Player(val name: String)
+class Player(val name: String) {
+  override def toString = name
+}
--- test/jt/katas/tennis/PlayerSpec.scala
+++ test/jt/katas/tennis/PlayerSpec.scala
@@ -6,10 +6,17 @@ import org.scalatest.FunSpec
  * Created by Jonathan Taylor on 05/01/2014.
  */
 class PlayerSpec extends FunSpec {
+
   describe("A player") {
+
     it("should have a name") {
       val player = new Player("Becker")
       assertResult("Becker")(player.name)
+    }
+
+    it("should use the player name as the string representation") {
+      val player = new Player("Becker")
+      assertResult("Becker")(player.toString)
     }
   }
 }
